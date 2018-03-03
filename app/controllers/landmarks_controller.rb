368b9411fd17e98a427d6f53208ff23fbeba433a
Alexander Urizar <urizaraj@gmail.com>
2018-03-03 17:57:47 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,15 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    haml :'landmarks/new'
+  end
 
+  get '/landmarks' do
+    @landmarks = Landmark.all.order(:name)
+    haml :'landmarks/index'
+  end
+
+  post '/landmarks' do
+    Landmark.create(params[:landmark])
+    redirect '/landmarks'
+  end
 end
